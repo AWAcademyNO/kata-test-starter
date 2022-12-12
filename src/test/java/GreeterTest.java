a0abf88b825987bcc7b6311642322f3455f19409
Stian Lindhom <stian@lindhom.no>
2022-12-12 22:23:18 +0100

    Initial commit

---
+++ src/test/java/GreeterTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GreeterTest {
+
+    @Test
+    public void shouldSayHello() {
+        Greeter greeter = new Greeter();
+
+        String greeting = greeter.sayHello();
+
+        assertEquals("Hello World!", greeting);
+    }
+}
